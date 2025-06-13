d84405ef033e9d8774ef25de02a2fd55ce0e4301
Iris Ibekwe <iris.ibekwe@civicactions.com>
2025-06-13 07:56:45 -0400

    Helper file to convert CSV to JSON.

---
+++ ai_migrations/utilities/csv_to_json.py
@@ -0,0 +1,25 @@
+import polars as pl
+import json
+
+def csv_to_json(input_csv: str):
+    """
+    Convert a CSV file to a JSON file.
+
+    :param input_csv: Path to the input CSV file.
+    :return: Path to the output JSON file.
+    """
+
+    # Read the CSV file using Polars
+    df = pl.read_csv(input_csv)
+
+    # Convert the DataFrame to a list of dictionaries
+    data = df.to_dicts()
+
+    # Define the output JSON file path
+    output_json = input_csv.replace('.csv', '.json')
+
+    # Write the data to a JSON file
+    with open(output_json, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+    return output_json
