13e8f39c2db4a5e76f2dfc21686584274c23a9b0
Iris Ibekwe <iris.ibekwe@civicactions.com>
2025-06-13 04:53:01 -0400

    Document audit cleanup and processing steps.

--- ai_migrations/main.py
+++ ai_migrations/main.py
@@ -1,6 +1,16 @@
 def main():
     print("Hello from ai-migrations!")
 
+    # -- Processing Steps -- 
+    #  INPUT: - `audit-inputs/**.csv`: A CSV file with crawled data.
+
+    # ✅ - `expand_json_csv.py`: Expand JSON columns in a CSV file.
+    # ✅ - `deduplicate_csv.py`: Deduplicate libraries in the expanded CSV file. (JS libraries, CSS libraries, content tags, etc.)
+    # ⚙️ - `crawl_analysis.py`: Extract informational columns, filter out non-HTML rows, Analyze the crawled data and extract descriptive columns.
+    # - `sidebar_analysis.py`: Analyze the sidebar structure of the crawled data.
+    # - `migration_groups.py`: Recommend migration groups based on the crawled data.
+
+    # OUTPUT: - `migration_groups.csv`: A CSV file with recommended migration groups for each site.
 
 if __name__ == "__main__":
     main()
